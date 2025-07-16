f4fba819a3a912fdcdf28b052fd8605a7ee26d9e
Merry <svgr1808@outlook.de>
2025-07-16 14:16:10 +0200

    Create plugin

---
+++ src/main/java/DeathSwapPlugin.java
@@ -0,0 +1,9 @@
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class DeathSwapPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getLogger().info("DeathSwapPlugin enabled!");
+    }
+}
