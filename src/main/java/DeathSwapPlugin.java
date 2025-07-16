262dbd56daab1207aa056d38da24a1372a97d342
Max <mausetot467@gmail.com>
2025-07-16 14:18:44 +0200

    Add getAllItems method

--- src/main/java/DeathSwapPlugin.java
+++ src/main/java/DeathSwapPlugin.java
@@ -1,4 +1,9 @@
+import org.bukkit.Material;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class DeathSwapPlugin extends JavaPlugin {
 
@@ -6,4 +11,11 @@ public class DeathSwapPlugin extends JavaPlugin {
     public void onEnable() {
         getLogger().info("DeathSwapPlugin enabled!");
     }
+
+    private List<Material> getAllItems() {
+        return Stream.of(Material.values())
+                .filter(Material::isItem)
+                .collect(Collectors.toList());
+    }
 }
+
