85e1996268d4361b8cbdfc0962b9dafe245782b3
Max <mausetot467@gmail.com>
2025-07-17 11:06:38 +0200

    refactor code

--- src/main/java/de/mlehrke/deathswap/command/InvseeCommand.java
+++ src/main/java/de/mlehrke/deathswap/command/InvseeCommand.java
@@ -8,7 +8,6 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.Inventory;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.HashSet;
@@ -47,7 +46,7 @@ public class InvseeCommand implements CommandExecutor {
             return true;
         }
 
-        if(player == target) {
+        if (player == target) {
             player.sendMessage("§aBitte versuche nicht dein eigenes Inventar zu öffnen...");
             return true;
         }
